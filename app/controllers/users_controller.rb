de5f17f4bd0726fab566820148405c5057baa6fc
ablakerose <bdecktor@gmail.com>
2019-09-24 09:11:53 -0500

    delete notes and clean up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-    #what routes do I need for login? 
+    
 
-    #the purpose of this route is to render the login page (which is the login form)
     get '/login' do
         erb :login
     end
